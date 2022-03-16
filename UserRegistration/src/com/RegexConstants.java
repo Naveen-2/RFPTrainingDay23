6e35f9023c5c6605b14684e8be79443c73d7145f
Naveen-2 <naveen2k15cse@gmail.com>
2022-03-16 22:47:25 +0530

    User Registration done till junit testing & generating manual exceptions in case of invalid input

---
+++ UserRegistration/src/com/RegexConstants.java
@@ -0,0 +1,58 @@
+package com;
+
+/**
+ * RegexConstants - class created to store all regex constants
+ * which can be used in any classes
+ */
+public class RegexConstants {
+
+    /*
+    Starting letter of first name should be capital letter, so ^[A-Z] is used
+    to fetch only one capital letter between A-Z.
+    Remaining letters of the name needs to be in small so [a-z] is used.
+    As the minimum number of characters need to be 3, {2,} quantifier is used to verify that
+    at least 2 letters from [a-z]. It's blank after comma(,) as it's not mentioned with
+    maximum character length for first name.
+     */
+    public static final String FIRST_NAME_REGEX = "^[A-Z][a-z]{2,}";
+
+    /*
+    Starting letter of last name should be capital letter, so ^[A-Z] is used
+    to fetch only one capital letter between A-Z.
+    Remaining letters of the name needs to be in small so [a-z] is used.
+    As the minimum number of characters need to be 3, {2,} quantifier is used to verify that
+    at least 2 letters from [a-z]. It's blank after comma(,) as it's not mentioned with
+    maximum character length for last name.
+     */
+    public static final String LAST_NAME_REGEX = "^[A-Z][a-z]{2,}";
+
+    /*
+    An email can start with any alphanumeric character or few special characters so
+    ^[a-zA-Z0-9_+&*-]+ is used. The '+' sign denotes the letters need to present one or more times.
+    The next part (?:pattern) can be entered or not entered. It's for zero or more times \\. is used to
+    escape '.' in the pattern.
+    After that '@' symbol is mandatory in an email which is directly entered. Then for a domain name with .
+    (?:[a-zA-Z0-9-]+\\.) is used at least one time
+    At the end, domain extension need to be 2 to 4 letters so [a-zA-Z]{2,7}$ is used.
+     */
+    public static final String EMAIL_REGEX = "^[a-zA-Z0-9_+&*-]+(?:\\.[a-zA-Z0-9_+&*-]+)*@(?:[a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,4}$";
+
+    /*
+    Mobile number is given a predefined structure in the UC as first 2 digits are country code
+    with a single space, so [0-9]{2} \\s is used.
+    Then the first number can be between 1-9 as we won't use 0 in starting of mobile number.
+    We can also restrict it to 6-9 as per the current usage of Indian numbers or any country usages
+    And then remaining 9 numbers will be between 0-9 for which quantifier {9} is used.
+     */
+    public static final String PHONE_NUMBER_REGEX = "[0-9]{2}\\s[1-9]{1}[0-9]{9}";
+
+    /*
+     A password needs to be at least 8 characters in it, so {8,0} is used to ensure that.
+     At least one special character. ^(?=.*[!@#$%^&*|'<>.-^*()%!])
+     And at least one number needs to be used. (?=.*[0-9])
+     And also at least one Upper case letter need to be entered for a valid password. (?=.*[A-Z])
+     There will not be any spaces and minimum 8 characters are used. [^\s]{8,}$
+     */
+    public static final String PASSWORD_REGEX = "^(?=.*[!@#$%^&*|'<>.-^*()%!])(?=.*[0-9])(?=.*[A-Z])[^\\s]{8,}$";
+
+}
---
+++ UserRegistration/src/com/UserRegistration.java
@@ -0,0 +1,117 @@
+package com;
+
+import java.util.Scanner;
+
+public class UserRegistration {
+
+    UserValidation userValidation = new UserValidation();
+    Scanner input = new Scanner(System.in);
+    String firstName = "";
+    String lastName = "";
+    String email = "";
+    String phoneNumber = "";
+    String password = "";
+
+    /**
+     * UserRegistration() - constructor to create object by getting input from user
+     * gets user registration details and stores in the variables
+     */
+    public UserRegistration() {
+
+        System.out.print("Enter First name: ");
+        setFirstName(input.nextLine());
+
+        System.out.print("Enter Last name: ");
+        setLastName(input.nextLine());
+
+        System.out.print("Enter email address: ");
+        setEmail(input.nextLine());
+
+        System.out.print("Enter phone number: ");
+        setPhoneNumber(input.nextLine());
+
+        System.out.print("Enter password: ");
+        setPassword(input.nextLine());
+    }
+
+    /**
+     * setFirstName - method to set value for firstName variable
+     * @param firstName - passes user input to function
+     *                  validates it by passing through userValidation object
+     *                  and then sets it to firstName variable of this class
+     *                  else asks to enter the first name again
+     */
+    public void setFirstName(String firstName) {
+        if(userValidation.isFirstNameValid(firstName))
+            this.firstName = firstName;
+        else{
+            System.out.print("Please enter first name again: ");
+            setFirstName(input.nextLine());
+        }
+    }
+
+    /**
+     *
+     * setLastName - method to set value for lastName variable
+     * @param lastName - passes user input to function
+     *                  validates it by passing through userValidation object
+     *                  and then sets it to lastName variable of this class
+     *                 else asks to enter last name again
+     */
+    public void setLastName(String lastName) {
+        if(userValidation.isLastNameValid(lastName))
+            this.lastName = lastName;
+        else{
+            System.out.print("Please enter last name again: ");
+            setLastName(input.nextLine());
+        }
+    }
+
+    /**
+     * setEmail - method to set value for email variable
+     * @param email - passes user email to function
+     *              validates it by passing through userValidation object
+     *              and then sets it to email variable of this class
+     *              else asks to enter email again
+     */
+    public void setEmail(String email) {
+        if(userValidation.isEmailValid(email))
+            this.email = email;
+        else{
+            System.out.print("Please enter email again: ");
+            setEmail(input.nextLine());
+        }
+    }
+
+    /**
+     * setPhoneNumber - method to set value for phoneNumber variable
+     * @param phoneNumber - passes user phone number to function
+     *              validates it by passing through userValidation object
+     *              and then sets it to phoneNumber variable of this class
+     *              else asks to enter phone number again
+     */
+    public void setPhoneNumber(String phoneNumber) {
+        if (userValidation.isPhoneNumberValid(phoneNumber))
+           this.phoneNumber = phoneNumber;
+        else{
+            System.out.print("Please enter phone number again: ");
+            setPhoneNumber(input.nextLine());
+        }
+    }
+
+    /**
+     * setPassword - method to set value for password variable
+     * @param password - passes user password to function
+     *              validates it by passing through userValidation object
+     *              and then sets it to password variable of this class
+     *              else asks to enter password again
+     */
+    public void setPassword(String password) {
+        if (userValidation.isPasswordValid(password))
+            this.password = password;
+        else {
+            System.out.print("Please enter password again: ");
+            setPassword(input.nextLine());
+        }
+    }
+}
