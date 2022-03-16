f32633c359c3b07d781665bccc7a0c671ee87699
Naveen-2 <naveen2k15cse@gmail.com>
2022-03-16 23:07:33 +0530

    Code refractored with lambda expressions in UserValidation class

--- UserRegistration/src/com/UserValidationTest.java
+++ UserRegistration/src/com/UserValidationTest.java
@@ -9,61 +9,61 @@ class UserValidationTest {
 
     @org.junit.jupiter.api.Test
     public void testFirstName_thenAssertionSucceeds() {
-            assertTrue(user.isFirstNameValid("Naveen"));
+            assertTrue(user.isFirstNameValid.myLambdaFunction("Naveen"));
             System.out.println("Valid First Name");
     }
 
     @org.junit.jupiter.api.Test
     public void testFirstName_thenAssertionFail() {
-        assertFalse(user.isFirstNameValid("naveen"));
+        assertFalse(user.isFirstNameValid.myLambdaFunction("naveen"));
         System.out.println("InvalidValid First Name");
     }
 
     @org.junit.jupiter.api.Test
     public void testLastName_thenAssertionSucceeds() {
-        assertTrue(user.isLastNameValid("Kumar"));
+        assertTrue(user.isLastNameValid.myLambdaFunction("Kumar"));
         System.out.println("Valid Last Name");
     }
 
     @org.junit.jupiter.api.Test
     public void testLastName_thenAssertionFail() {
-        assertFalse(user.isLastNameValid("kumar"));
+        assertFalse(user.isLastNameValid.myLambdaFunction("kumar"));
         System.out.println("Invalid Last Name");
     }
 
     @org.junit.jupiter.api.Test
     public void testEmail_thenAssertionSucceeds() {
-        assertTrue(user.isEmailValid("abc@gmail.com"));
+        assertTrue(user.isEmailValid.myLambdaFunction("abc@gmail.com"));
         System.out.println("Valid Email");
     }
 
     @org.junit.jupiter.api.Test
     public void testEmail_thenAssertionFail() {
-        assertFalse(user.isEmailValid("abc@gmail@.com@"));
+        assertFalse(user.isEmailValid.myLambdaFunction("abc@gmail@.com@"));
         System.out.println("Invalid Email");
     }
 
     @org.junit.jupiter.api.Test
     public void testMobileNumber_thenAssertionSucceeds() {
-        assertTrue(user.isPhoneNumberValid("91 9876543210"));
+        assertTrue(user.isPhoneNumberValid.myLambdaFunction("91 9876543210"));
         System.out.println("Valid Mobile Number");
     }
 
     @org.junit.jupiter.api.Test
     public void testMobileNumber_thenAssertionFail() {
-        assertFalse(user.isPhoneNumberValid("910876543210"));
+        assertFalse(user.isPhoneNumberValid.myLambdaFunction("910876543210"));
         System.out.println("Invalid Mobile Number");
     }
 
     @org.junit.jupiter.api.Test
     public void testPassword_thenAssertionSucceeds() {
-        assertTrue(user.isPasswordValid("Abcd@1234"));
+        assertTrue(user.isPasswordValid.myLambdaFunction("Abcd@1234"));
         System.out.println("Valid Password");
     }
 
     @org.junit.jupiter.api.Test
     public void testPassword_thenAssertionFail() {
-        assertFalse(user.isPasswordValid("bbb"));
+        assertFalse(user.isPasswordValid.myLambdaFunction("bbb"));
         System.out.println("Invalid Password");
     }
 
